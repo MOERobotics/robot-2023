8995c48707a5e3f304c2c21d63453a2e75be8143
Malti <maltimjohn1@gmail.com>
2023-01-11 20:36:11 -0500

    swerve drive beginning

---
+++ src/main/java/frc/robot/teleop/DriveCode.java
@@ -0,0 +1,17 @@
+package frc.robot.teleop;
+
+import frc.robot.generic.GenericRobot;
+
+public class DriveCode extends GenericTeleop{
+
+    @Override
+    public void teleopInit(GenericRobot robot){
+
+    }
+
+    @Override
+    public void teleopPeriodic(GenericRobot robot){
+
+    }
+
+}
---
+++ src/main/java/frc/robot/teleop/GenericTeleop.java
@@ -0,0 +1,16 @@
+package frc.robot.teleop;
+
+import frc.robot.generic.GenericRobot;
+
+public abstract class GenericTeleop {
+
+    public void teleopInit(GenericRobot robot) {
+
+    }
+
+    public void teleopPeriodic(GenericRobot robot) {
+
+    }
+
+
+}
