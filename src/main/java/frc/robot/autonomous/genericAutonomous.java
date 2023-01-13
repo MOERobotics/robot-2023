257561953b6e466a5bf93daa628edf18b9aa6f5a
Jame <wujames989@gmail.com>
2023-01-12 19:26:48 -0500

    Started generic autonomous code

---
+++ src/main/java/frc/robot/autonomous/genericAutonomous.java
@@ -0,0 +1,15 @@
+package frc.robot.autonomous;
+
+import frc.robot.generic.GenericRobot;
+
+public abstract class genericAutonomous {
+    public int autonomousStep = 0;
+
+    public void autonomousInit(GenericRobot robot){
+
+    }
+
+    public void autonomousPeriodic(GenericRobot robot){
+
+    }
+}
