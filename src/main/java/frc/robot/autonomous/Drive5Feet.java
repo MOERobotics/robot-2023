e5d5ed384617e3f515e535dc78abfcee5fafeb86
Jame <wujames989@gmail.com>
2023-01-25 18:54:41 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHH, Daniel told me to push

---
+++ src/main/java/frc/robot/autonomous/Drive5Feet.java
@@ -0,0 +1,16 @@
+package frc.robot.autonomous;
+
+import frc.robot.generic.GenericRobot;
+
+public class Drive5Feet extends genericAutonomous{
+
+    public int autonomousStep = 0;
+
+    public void autonomousInit(GenericRobot robot){
+
+    }
+
+    public void autonomousPeriodic(GenericRobot robot){
+
+    }
+}
