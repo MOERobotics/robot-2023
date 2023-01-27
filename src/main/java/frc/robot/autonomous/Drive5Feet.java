7884a560d79037632f9948a2833fc3ec37cb2544
S-OS <33400784+sosudo@users.noreply.github.com>
2023-01-26 20:27:51 -0500

    finished the trajectory function still need to write getters for dxra and dyran

--- src/main/java/frc/robot/autonomous/Drive5Feet.java
+++ src/main/java/frc/robot/autonomous/Drive5Feet.java
@@ -1,7 +1,12 @@
 package frc.robot.autonomous;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.*;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.generic.GenericRobot;
+import frc.robot.generic.swerveBot;
 
 public class Drive5Feet extends genericAutonomous {
     Pose2d current_position;
@@ -10,13 +15,16 @@ public class Drive5Feet extends genericAutonomous {
     double start_distance;
     double default_power;
     double start_time;
-
+    double xspd = .5;
+    double yspd = .5;
+    double rspd = 0;
+    @Override
     public void autonomousInit(GenericRobot robot){
         autonomousStep = 0;
         start_time = System.currentTimeMillis();
         //TODO: add pid stuff
     }
-
+    @Override
     public void autonomousPeriodic(GenericRobot robot){
         switch(autonomousStep){
             case 0: //reset
@@ -25,13 +33,37 @@ public class Drive5Feet extends genericAutonomous {
                 }
                 break;
             case 1: //drive forward 5ft
-                current_position = robot.getPose();
-                //TODO: add getPose stuff
                 autonomousStep += 1;
                 break;
             case 2: //stop
 
                 break;
         }
+    }
+    @Override
+    public void trajectoryCalc(GenericRobot robot, Pose2d origin, Pose2d dest) {
+        Pose2d diff = diff(robot, origin, dest);
+        double dx = diff.getX();
+        double dy = diff.getY();
+        xspd = (dx != dxran) ? 0.5 : 0;
+        yspd = (dy != dyran) ? 0.5 : 0;
+        ChassisSpeeds targetChassisSpeeds = new ChassisSpeeds(xspd, yspd, rspd);
+        SwerveModuleState[] moduleStates = robot.kinematics().toSwerveModuleStates(targetChassisSpeeds);
+        SwerveModuleState frontLeftState = moduleStates[0],
+                frontRightState = moduleStates[1],
+                backLeftState = moduleStates[2],
+                backRightState = moduleStates[3];
+
+        frontLeftState = SwerveModuleState.optimize(frontLeftState, Rotation2d.fromDegrees(robot.getPivotLeftMotorA()));
+        frontRightState = SwerveModuleState.optimize(frontRightState, Rotation2d.fromDegrees(robot.getPivotRightMotorA()));
+        backLeftState = SwerveModuleState.optimize(backLeftState, Rotation2d.fromDegrees(robot.getPivotLeftMotorB()));
+        backRightState = SwerveModuleState.optimize(backRightState, Rotation2d.fromDegrees(robot.getPivotRightMotorB()));
+
+        robot.swerve(frontLeftState, frontRightState, backLeftState, backRightState);
+    }
+
+    @Override
+    public Pose2d diff(GenericRobot robot, Pose2d one, Pose2d two) {
+             return new Pose2d(one.getX()-two.getX(), one.getY()-two.getY(), Rotation2d.fromDegrees(0));
     }
 }
--- src/main/java/frc/robot/autonomous/genericAutonomous.java
+++ src/main/java/frc/robot/autonomous/genericAutonomous.java
@@ -1,5 +1,7 @@
 package frc.robot.autonomous;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import frc.robot.generic.GenericRobot;
 
 public abstract class genericAutonomous {
@@ -10,6 +12,12 @@ public abstract class genericAutonomous {
     }
 
     public void autonomousPeriodic(GenericRobot robot){
+    }
 
+    public void trajectoryCalc(GenericRobot robot, Pose2d origin, Pose2d dest) {
+
+    }
+    public Pose2d diff(GenericRobot robot, Pose2d one, Pose2d two) {
+        return new Pose2d();
     }
 }
