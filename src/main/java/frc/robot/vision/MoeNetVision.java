d27acbba04f866050b72bec2e997ca528064266d
RCode2 <rohan.sivva@gmail.com>
2023-01-29 12:51:18 -0500

    Created moe_net2023

---
+++ src/main/java/frc/robot/vision/MoeNetVision.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.vision;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public final class MoeNetVision {
+
+    NetworkTableEntry poseEntry;
+
+    public MoeNetVision(NetworkTableInstance nt){
+        var sd = nt.getTable("SmartDashboard");
+        poseEntry = sd.getEntry("pose");
+    }
+    public Pose2d getPose(){
+        var pose = poseEntry.getDoubleArray(new double[0]);
+        if (pose.length == 0){
+            return null;
+        }
+
+        double x = pose[0];
+        double y = pose[1];
+        double z = pose[2];
+        double pitch = pose[3];
+        double roll = pose[4];
+        double yaw = pose[5];
+
+        var rotation = new Rotation2d(yaw);
+        var pose2d = new Pose2d(x, y, rotation);
+        return pose2d;
+    }
+}
