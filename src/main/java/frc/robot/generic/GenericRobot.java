2a57494700c5cf88cf4ec9b19a2ba194afa2843f
malti <maltimjohn1@gmail.com>
2023-02-05 13:05:42 -0500

    Basic arm code developed. Odometry issue resolved.

--- src/main/java/frc/robot/generic/GenericRobot.java
+++ src/main/java/frc/robot/generic/GenericRobot.java
@@ -267,6 +267,8 @@ public abstract class GenericRobot {
         return defaultPose;
     }
 
+    public void setPose(){}
+
 
     public void resetStartHeading() {
         startHeading = getYaw();
@@ -281,4 +283,34 @@ public abstract class GenericRobot {
         startPivots = new double[] {getPivotLeftMotorA(), getPivotRightMotorA(),
                 getPivotLeftMotorB(), getPivotRightMotorB()};
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////Collector Code
+
+    public void setBottomRollerPower(double power){}
+    public void setTopRollerPower(double power){}
+
+    public void collect(double rpm){}
+    public void setBottomRollerRPM(double rpm){}
+    public void setTopRollerRPM(double rpm){}
+
+    public void setTopRollerPosPower(double power){}
+
+    public double getTopRollerPosition(){return 0;}
+
+    public boolean cargoInCollector(){return false;}
+
+    ////////////////////////////////////////////////////////////////////////////////////Arm Code
+
+    public void rightArmPower(double power){}
+    public void leftArmPower(double power){}
+    public void moveArm(double power){}
+    public double getArmPosition(){return 0;}
+
+    ///////////////////////////////////////////////////////////////////////////////////Gripper Code
+    public void openGripper(){}
+    public void closeGripper(){}
+
+    public boolean gripperIsOpen(){return false;}
+
+
 }
