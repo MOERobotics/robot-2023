7d7917cb81358281f367737ac812c69095d2fd70
EGoat123 <saiarunjayanthi@gmail.com>
2023-02-19 13:30:22 -0500

    Labelling Javadocs (very epic)

--- src/main/java/frc/robot/vision/EstimatedRobotPose.java
+++ src/main/java/frc/robot/vision/EstimatedRobotPose.java
@@ -3,8 +3,15 @@ package frc.robot.vision;
 import edu.wpi.first.math.geometry.Pose3d;
 
 public class EstimatedRobotPose {
-    Pose3d estimatedPose;
+    /**
+     * A Pose3D of our robot in field space. This data is in meters. If it isn't it will be converted by this point.
+     */
+    public final Pose3d estimatedPose;
     double latencyMS;
+
+    /**
+     * Bigger is better not real units
+     */
     double accuracy;
     public EstimatedRobotPose(Pose3d estimatedPose, double latencyMS, double accuracy) {
         this.estimatedPose = estimatedPose;
--- src/main/java/frc/robot/vision/NetworkCamera.java
+++ src/main/java/frc/robot/vision/NetworkCamera.java
@@ -2,6 +2,14 @@ package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Pose3d;
 
+/**
+ * Camera in network tables
+ */
 public interface NetworkCamera {
+    /**
+     * Gives a pose. If we have no data from the camera, or we are not receiving new data,
+     * we set to null.
+     * @return EstimatedRobotPose, a combination of Pose3D, timestamps, and accuracy
+     */
     EstimatedRobotPose getPose();
 }
