8a7ec330dfa5fdec746c5e59a0c4c10e40d6b99b
RCode2 <rohan.sivva@gmail.com>
2023-02-20 18:51:37 -0500

    Fixed ButtonBox class to have a setter function and two getter functions

--- src/main/java/frc/robot/helpers/ButtonBox.java
+++ src/main/java/frc/robot/helpers/ButtonBox.java
@@ -107,40 +107,44 @@ public class ButtonBox {
         if(joystick.getRawButtonPressed(1)) {
             currentRow = 1;
         }
-        if(joystick.getRawButtonPressed(2)) {
+        else if(joystick.getRawButtonPressed(2)) {
             currentRow = 2;
         }
-        if(joystick.getRawButtonPressed(3)) {
+        else if(joystick.getRawButtonPressed(3)) {
             currentRow = 3;
         }
-        if(joystick.getRawButtonPressed(4)) {
+        else if(joystick.getRawButtonPressed(4)) {
             currentRow = 4;
         }
-        if(joystick.getRawButtonPressed(5)) {
+        else if(joystick.getRawButtonPressed(5)) {
             currentRow = 5;
         }
-        if(joystick.getRawButtonPressed(6)) {
+        else if(joystick.getRawButtonPressed(6)) {
             currentRow = 6;
         }
-        if(joystick.getRawButtonPressed(7)) {
+        else if(joystick.getRawButtonPressed(7)) {
             currentRow = 7;
         }
-        if(joystick.getRawButtonPressed(8)) {
+        else if(joystick.getRawButtonPressed(8)) {
             currentRow = 8;
         }
-        if(joystick.getRawButtonPressed(9)) {
+        else if(joystick.getRawButtonPressed(9)) {
             currentRow = 9;
         }
-        currentRow = 0;
+        else {
+            currentRow = 0;
+        }
         if(!joystick.getRawButtonPressed(10) && !joystick.getRawButtonPressed(11)) {
             currentHeight = height.MIDDLE;
         }
-        if(!joystick.getRawButtonPressed(10) && joystick.getRawButtonPressed(11)) {
+        else if(!joystick.getRawButtonPressed(10) && joystick.getRawButtonPressed(11)) {
             currentHeight = height.LOW;
         }
-        if(joystick.getRawButtonPressed(10) && !joystick.getRawButtonPressed(11)) {
+        else if(joystick.getRawButtonPressed(10) && !joystick.getRawButtonPressed(11)) {
             currentHeight = height.HIGH;
         }
-        currentHeight = height.NONE;
+        else {
+            currentHeight = height.NONE;
+        }
     }
 }
