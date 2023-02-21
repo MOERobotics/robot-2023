ef2486e9751c7dd6d353da47331c5693fa93ffcd
malti <maltimjohn1@gmail.com>
2023-02-20 20:33:36 -0500

    Added a ramp up/ ramp down

--- src/main/java/frc/robot/helpers/AutoCodeLines.java
+++ src/main/java/frc/robot/helpers/AutoCodeLines.java
@@ -32,4 +32,33 @@ public class AutoCodeLines {
     public static double getVelocityY(Point p, Point q, double s){
         return getSlopeY(p,q);
     }
+
+    public static double getdS(double distance, double timeToMax, double averageSpeed, double t){
+        double time = distance/averageSpeed;
+        double maxSpeed = averageSpeed/(time-timeToMax);
+        if (t <= timeToMax){
+            return getPositionY(new Point(0,0), new Point(timeToMax, maxSpeed), t);
+        }
+        else if (t <= time-timeToMax){
+            return maxSpeed;
+        }
+        else{
+            return getPositionY(new Point(time-timeToMax, maxSpeed), new Point(time, 0), t-(time-timeToMax));
+        }
+    }
+
+    public static double getS(double distance, double timeToMax, double averageSpeed, double t){
+        double time = distance/averageSpeed;
+        double maxSpeed = averageSpeed/(time-timeToMax);
+        if (t <= timeToMax){
+            return getPositionY(new Point(0,0), new Point(timeToMax, maxSpeed), t)*t/2;
+        }
+        else if (t <= time-timeToMax){
+            return timeToMax*maxSpeed/2 + maxSpeed*(t-timeToMax);
+        }
+        else{
+            return timeToMax*maxSpeed + maxSpeed * (time-2*timeToMax)
+                    - getPositionY(new Point(time-timeToMax, maxSpeed), new Point(time, 0), t-(time-timeToMax))*(time-t)/2;
+        }
+    }
 }
