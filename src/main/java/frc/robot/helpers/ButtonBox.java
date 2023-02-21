3996f186cf3123de8276304888e5878e68dea112
RCode2 <rohan.sivva@gmail.com>
2023-02-20 19:01:22 -0500

    Fixed ButtonBox class to not reset currentRow and currentHeight

--- src/main/java/frc/robot/helpers/ButtonBox.java
+++ src/main/java/frc/robot/helpers/ButtonBox.java
@@ -107,44 +107,38 @@ public class ButtonBox {
         if(joystick.getRawButtonPressed(1)) {
             currentRow = 1;
         }
-        else if(joystick.getRawButtonPressed(2)) {
+        if(joystick.getRawButtonPressed(2)) {
             currentRow = 2;
         }
-        else if(joystick.getRawButtonPressed(3)) {
+        if(joystick.getRawButtonPressed(3)) {
             currentRow = 3;
         }
-        else if(joystick.getRawButtonPressed(4)) {
+        if(joystick.getRawButtonPressed(4)) {
             currentRow = 4;
         }
-        else if(joystick.getRawButtonPressed(5)) {
+        if(joystick.getRawButtonPressed(5)) {
             currentRow = 5;
         }
-        else if(joystick.getRawButtonPressed(6)) {
+        if(joystick.getRawButtonPressed(6)) {
             currentRow = 6;
         }
-        else if(joystick.getRawButtonPressed(7)) {
+        if(joystick.getRawButtonPressed(7)) {
             currentRow = 7;
         }
-        else if(joystick.getRawButtonPressed(8)) {
+        if(joystick.getRawButtonPressed(8)) {
             currentRow = 8;
         }
-        else if(joystick.getRawButtonPressed(9)) {
+        if(joystick.getRawButtonPressed(9)) {
             currentRow = 9;
-        }
-        else {
-            currentRow = 0;
         }
         if(!joystick.getRawButtonPressed(10) && !joystick.getRawButtonPressed(11)) {
             currentHeight = height.MIDDLE;
         }
-        else if(!joystick.getRawButtonPressed(10) && joystick.getRawButtonPressed(11)) {
+        if(!joystick.getRawButtonPressed(10) && joystick.getRawButtonPressed(11)) {
             currentHeight = height.LOW;
         }
-        else if(joystick.getRawButtonPressed(10) && !joystick.getRawButtonPressed(11)) {
+        if(joystick.getRawButtonPressed(10) && !joystick.getRawButtonPressed(11)) {
             currentHeight = height.HIGH;
-        }
-        else {
-            currentHeight = height.NONE;
         }
     }
 }
