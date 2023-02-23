c22bd4f6ec207e76f428e1453b3e0695f7271a9b
EGoat123 <saiarunjayanthi@gmail.com>
2023-02-22 20:17:56 -0500

    xyz fixed for oak

--- src/main/java/frc/robot/vision/OakCamera.java
+++ src/main/java/frc/robot/vision/OakCamera.java
@@ -64,7 +64,7 @@ public class OakCamera implements NetworkCamera{
             var y= detections[i+1];
             var z= detections[i+2];
             Detection.Cargo cargoType = Detection.Cargo.values()[(int)detections[i+3]];
-            Detection newDebt = new Detection(x,y,z,cargoType);
+            Detection newDebt = new Detection(x,z,y,cargoType);
             debt.add(newDebt);
         }
 
