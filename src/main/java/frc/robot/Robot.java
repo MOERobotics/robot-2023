84ad25728d580191e49341b0c028687733f53e20
malti <maltimjohn1@gmail.com>
2023-02-28 09:43:14 -0500

    Auto selection in disabled

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,26 +4,25 @@
 
 package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.autonomous.*;
 import frc.robot.generic.GenericRobot;
 import frc.robot.generic.TherMOEDynamic;
-import frc.robot.generic.swerveBot;
 import frc.robot.teleop.DriveCode;
-import frc.robot.teleop.ArmCode;
 import frc.robot.teleop.GenericTeleop;
 import frc.robot.vision.MoeNetVision;
 import edu.wpi.first.wpilibj.DriverStation;
 
 
 public class Robot extends TimedRobot {
-  public static final GenericTeleop
-          driveCode = new DriveCode();
+  Joystick autoSelect = new Joystick(0);
+  public static final genericAutonomous
+        A1CDock = new A1CDock(),
+        A1B2C = new A1B2C(),
+        ExitAndEngage = new ExitAndEngage();
 
   genericAutonomous autonomous = new A1CDock();
   GenericTeleop teleop = new DriveCode();
@@ -55,7 +54,7 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
 
    SmartDash.Dashboard(robot);
-
+   SmartDashboard.putString("autonomous", autonomous.getClass().getName());
    OurAllianceColor = DriverStation.getAlliance();
 
    if (OurAllianceColor == DriverStation.Alliance.Red) {
@@ -100,6 +99,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledPeriodic() {
+   if (autoSelect.getRawButtonPressed(1)) autonomous = A1CDock;
+   if (autoSelect.getRawButtonPressed(2)) autonomous = A1B2C;
+   if (autoSelect.getRawButtonPressed(3)) autonomous = ExitAndEngage;
    vision.disabledPeriodic();
   }
 
